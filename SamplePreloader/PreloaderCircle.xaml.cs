f5a63a77749c8601b885ec6180c5aa24987bded3
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-07-31 17:55:24 -0300

    incluindo loader com circulos

---
+++ SamplePreloader/PreloaderCircle.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SamplePreloader
+{
+    /// <summary>
+    /// Interaction logic for PreloaderCircle.xaml
+    /// </summary>
+    public partial class PreloaderCircle : UserControl
+    {
+        public PreloaderCircle()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Storyboard storyboard = this.FindResource("animation") as Storyboard;
+            storyboard.Begin();
+        }
+    }
+}
